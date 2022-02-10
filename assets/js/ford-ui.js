266938955c3d60371451ff75743d1fe957d00649
opacteam <mike@minisisinc.com>
2022-02-10 16:31:45 +0700

    Import new ford design files

---
+++ assets/js/ford-ui.js
@@ -0,0 +1,134 @@
+$(document).ready(function () {
+  let filter = new Filter();
+
+  // Toggle the collpase filter
+  $(".filterButton").on("click", function () {
+    $("#filterCollapse").collapse("toggle");
+  });
+
+  // Expand filter on click
+  $(".filterSelect").on("click", function () {
+    filter.openFilter($(this));
+  });
+
+  $(".filterDropdown ul li").on("click", function () {
+    filter.selectFilterValue($(this));
+  });
+
+  // When clicking out of the filter, option is closed
+  $(document).mouseup(function (e) {
+    let container = $(".filterSelect");
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
+      filter.closeAllFilter();
+    }
+  });
+
+  // Color Filter Handler
+
+  $(".colorFilter").on("click", function (e) {
+    $(".colorFilter").each(function (e) {
+      $(this).removeClass("selectedColorFilter");
+    });
+    let color = $(this).data("color");
+    if (filter.color !== color) {
+      $(this).addClass("selectedColorFilter");
+      filter.color = color;
+    } else {
+      filter.resetColor();
+    }
+  });
+
+  // Asset Filter Handler
+
+  $(".assetTypeFilter").on("change", function (e) {
+    let checked = $(this).is(":checked");
+    let assetType = $(this).data("asset");
+    checked
+      ? filter.addAssetType(assetType)
+      : filter.removeAssetType(assetType);
+  });
+});
+
+class Filter {
+  constructor() {
+    this.year = "";
+    this.make = "";
+    this.model = "";
+    this.color = "";
+    this.assetType = [];
+    this.keyword = "";
+  }
+
+  resetAll() {
+    this.year = "";
+    this.make = "";
+    this.model = "";
+    this.color = "";
+    this.assetType = [];
+    this.keyword = "";
+  }
+
+  resetKeyword() {
+    this.keyword = "";
+  }
+
+  resetYear() {
+    this.year = "";
+  }
+
+  resetMake() {
+    this.make = "";
+  }
+
+  resetModel() {
+    this.model = "";
+  }
+
+  resetColor() {
+    this.color = "";
+  }
+
+  resetAssetType() {
+    this.assetType = [];
+  }
+
+  removeAssetType(t) {
+    this.assetType = this.assetType.filter((e) => e !== t);
+  }
+
+  addAssetType(t) {
+    this.assetType.push(t);
+  }
+
+  openFilter(filterDOM) {
+    let expand = filterDOM.find($(".expand"));
+    let dropdown = filterDOM.find($(".filterDropdown"));
+    expand.text(
+      dropdown.hasClass("hideDropdown") ? "expand_less" : "expand_more"
+    );
+    this.closeAllFilter(filterDOM);
+    dropdown.toggleClass("hideDropdown");
+  }
+
+  selectFilterValue(filterOptionDOM) {
+    let value = filterOptionDOM.text();
+    let filterText = filterOptionDOM
+      .parent()
+      .parent()
+      .parent()
+      .find(".filterText");
+
+    let filter = filterText.data("filter").toLowerCase();
+    filterText.text(value);
+    this[filter] = value;
+  }
+
+  closeAllFilter(excl) {
+    let dropdown = $(".filterDropdown");
+    dropdown.each(function () {
+      if (!$(this).hasClass("hideDropdown")) {
+        $(this).addClass("hideDropdown");
+      }
+    });
+  }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      backgroundImage: {
+        test: "url('https://i.imgur.com/ZVy1nu8.png')",
+      },
+      boxShadowColor: {
+        headerShadow:
+          "0 30px 30px 0 rgba(0,0,0,0.09), 0 20px 20px 0 rgba(0,0,0,0.1), 0 10px 10px 0 rgba(0,0,0,0.1)",
+      },
+      height: {
+        "90px": "90px",
+      },
+      colors: {
+        primary: "#00095B",
+      },
+    },
+  },
+  plugins: [],
+};
