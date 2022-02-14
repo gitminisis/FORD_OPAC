0055fbbc76661b33c54555d54e7c41f01797aae0
opacteam <mike@minisisinc.com>
2022-02-15 04:33:43 +0700

    install tailwind cli

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,8 @@
-tailwind.config = {
+module.exports = {
+  content: ["./*.{html,js}", "./includes/html/**/*.{html,js}"],
   theme: {
-    extend: {
-      backgroundImage: {
-        test: "url('https://i.imgur.com/ZVy1nu8.png')",
-      },
-      boxShadowColor: {
-        headerShadow:
-          "0 30px 30px 0 rgba(0,0,0,0.09), 0 20px 20px 0 rgba(0,0,0,0.1), 0 10px 10px 0 rgba(0,0,0,0.1)",
-      },
-      height: {
-        "90px": "90px",
-      },
-      colors: {
-        primary: "#00095B",
-      },
-    },
+    extend: {},
   },
   plugins: [],
+  
 };
