72dad2d50a113b6707602417eafd72dd1dd32535
opacteam <mike@minisisinc.com>
2022-02-15 04:36:23 +0700

    update gitignore

--- Legacy FORD OPAC/M2A/src/context/index.js
+++
@@ -1,4 +0,0 @@
-import React from "react";
-export const OnPageContext = React.createContext("1");
-export const PageViewContext = React.createContext("list");
-export const BreadcrumbContext = React.createContext(["Home"]);
