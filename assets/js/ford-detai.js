ca6b6c478f50fd5f8bb9a9599e8f8342690aa8e2
opacteam <mike@minisisinc.com>
2022-04-05 03:43:24 +0700

    finished the carousel

--- assets/js/ford-detai.js
+++ assets/js/ford-detai.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
-  let imageTest = '<div class="hidden_fields" hidden=""> <span class="a_media_type">Image</span><span class="a_media_low_res"> https://titanapi.minisisinc.com/api/links/c29a9048c4864d89915b29f4f39330e4/uui d/666d78d0afd2487eac7eac3478ce5fa3/access</span><span class="a_media_thumb"> https://titanapi.minisisinc.com/api/links/c29a9048c4864d89915b29f4f39330e4/uui d/666d78d0afd2487eac7eac3478ce5fa3/thumbnail</span> <span class="a_media_type">Image</span><span class="a_media_low_res"> https://titanapi.minisisinc.com/api/links/c29a9048c4864d89915b29f4f39330e4/uui d/b0655a296f7940b383f58fdd01a18ddc/access</span><span class="a_media_thumb"> https://titanapi.minisisinc.com/api/links/c29a9048c4864d89915b29f4f39330e4/uui d/b0655a296f7940b383f58fdd01a18ddc/thumbnail</span> <span class="a_media_type">Image</span><span class="a_media_low_res"> https://titanapi.minisisinc.com/api/links/c29a9048c4864d89915b29f4f39330e4/uui d/3919f29d405841fbaacb0b01e9a520de/access</span><span class="a_media_thumb"> https://titanapi.minisisinc.com/api/links/c29a9048c4864d89915b29f4f39330e4/uui d/3919f29d405841fbaacb0b01e9a520de/thumbnail</span> </div>'
+  let imageTest = '<div class="hidden_fields" hidden=""> <span class="a_media_type">Image</span><span class="a_media_low_res"> https://titanapi.minisisinc.com/api/links/c29a9048c4864d89915b29f4f39330e4/uui d/666d78d0afd2487eac7eac3478ce5fa3/access</span><span class="a_media_thumb"> https://titanapi.minisisinc.com/api/links/c29a9048c4864d89915b29f4f39330e4/uui d/666d78d0afd2487eac7eac3478ce5fa3/thumbnail</span> <span class="a_media_type">Image</span><span class="a_media_low_res"> https://titanapi.minisisinc.com/api/links/c29a9048c4864d89915b29f4f39330e4/uui d/666d78d0afd2487eac7eac3478ce5fa3/access</span><span class="a_media_thumb"> https://titanapi.minisisinc.com/api/links/c29a9048c4864d89915b29f4f39330e4/uui d/666d78d0afd2487eac7eac3478ce5fa3/thumbnail</span><span class="a_media_type">Image</span><span class="a_media_low_res"> https://titanapi.minisisinc.com/api/links/c29a9048c4864d89915b29f4f39330e4/uui d/666d78d0afd2487eac7eac3478ce5fa3/access</span><span class="a_media_thumb"> https://titanapi.minisisinc.com/api/links/c29a9048c4864d89915b29f4f39330e4/uui d/666d78d0afd2487eac7eac3478ce5fa3/thumbnail</span></div>'
   if (document.getElementById("detail")) {
-    // $('body').append(imageTest)
+    $('body').append(imageTest)
     const detail = new Detail();
     detail.init();
 
@@ -45,10 +45,15 @@ class Detail extends Report {
     }
   }
 
-  initLightgallery() {
-    lightGallery(document.getElementById('detail_media'), {
-      selector: '.item'
-    });
+  initLightgallery(multi = true) {
+    if (multi) {
+      lightGallery(document.querySelector('.slides'));
+    }
+    else {
+      lightGallery(document.getElementById('detail_media'), {
+        selector: '.item'
+      });
+    }
 
   }
 
@@ -144,7 +149,7 @@ class Detail extends Report {
       let { mediaType, mediaLowRes, mediaThumb } = assets[0];
       if (mediaType === 'Image') {
         detailMediaDOM.append(`<div class="item" data-src=${mediaLowRes}><img class="h-[80%] mx-[auto]" src=${mediaThumb} /> </div>`)
-        detail.initLightgallery();
+        detail.initLightgallery(false);
       }
       else if (mediaType === 'Textual') {
         detailMediaDOM.append(`<div class="item" data-src=${mediaLowRes}><a target="_blank" href=${mediaLowRes}><img class="h-[80%] mx-[auto]" src=${mediaThumb} /></a> </div>`)
@@ -190,7 +195,7 @@ class Detail extends Report {
 
     this.setReturnSummaryURL();
     this.setTotalRecord();
-    this.initLightgallery();
+
     this.initDetailAssets();
     this.setMediaView();
     this.initDownloadSection();
