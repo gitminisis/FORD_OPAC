5de8e72e845f6aed5e5955197cf1e64efb3f3331
opacteam <mike@minisisinc.com>
2023-08-15 04:11:59 -0400

    push live serv

--- assets/js/ford-home.js
+++ assets/js/ford-home.js
@@ -4,21 +4,21 @@ $(document).ready(function () {
         // const LATEST_ARCHIVE_REFD = ['AR-96-212010.5423', 'AR-2008-8.1.4.555', 'AR-2008-8.1.2.2049'];
         const LATEST_ARCHIVE_RECORD = [
             {
-                mediaThumb: "https://fordheritagevaultmedia.com/api/links/c29a9048c4864d89915b29f4f39330e4/uuid/ccb27c69697b441bbbac1d3e74283bf8/thumbnail",
-                refd: "AR-2008-8.1.2.550",
+                mediaThumb: "https://fordheritagevaultmedia.com/api/links/c29a9048c4864d89915b29f4f39330e4/uuid/9d0507cf6150430eaf1ad2ac780cf6c2/thumbnail",
+                refd: "AR-2008-8.1.13.410",
                 scope: undefined,
-                title: "1940 Ford Soybean Plant at Rouge exterior neg C72 "
+                title: "2003 Ford Model U concept car interior CN335041-337"
             },
             {
-                mediaThumb: "https://fordheritagevaultmedia.com/api/links/c29a9048c4864d89915b29f4f39330e4/uuid/f8b0090dfd2543cca0528e47dc9975a6/thumbnail",
-                refd: "AR-2008-8.1.10.7939",
+                mediaThumb: "https://fordheritagevaultmedia.com/api/links/c29a9048c4864d89915b29f4f39330e4/uuid/2bd63756fcb641d4b4de5a01e24db660/thumbnail",
+                refd: "AR-96-212010.6880",
                 scope: undefined,
-                title: "1952 Ford La Boca Assembly Plant neg 75E85"
+                title: "c 1960 Levacar concept brochure AR-2004-72-12262"
             }, {
-                mediaThumb: "https://fordheritagevaultmedia.com/api/links/c29a9048c4864d89915b29f4f39330e4/uuid/8c3ef82fc1b744d9bd668ff0e110ac30/thumbnail",
-                refd: "AR-2008-8.1.10.8001",
+                mediaThumb: "https://fordheritagevaultmedia.com/api/links/c29a9048c4864d89915b29f4f39330e4/uuid/c9219a4a89fb4ccd8b0b7ae49f614f23/thumbnail",
+                refd: "AR-2008-8.1.11.2008",
                 scope: undefined,
-                title: "2003 Ford Focus in road simulator at Dunton Research and Engineering Center",
+                title: "1962 Ford Mustang I concept on scenic road neg CN2042-1",
             }
         ]
         LATEST_ARCHIVE_RECORD.map((e, i) => new LatestArchive(e, $('.latestArchive').eq(i)).initUIManual(e))
@@ -26,42 +26,41 @@ $(document).ready(function () {
         const TRENDING_NOW_REFD = ['AR-65-90.1249.7', 'AR-2008-8.1.2.2447', 'AR-96-212010.5443', 'AR-2008-8.1.2.4143', 'AR-2008-8.1.7.257', 'AR-96-212010.6325', 'AR-96-212010.5592'];
         const TRENDING_NOW_RECORD = [
             {
-                mediaThumb: "https://fordheritagevaultmedia.com/api/links/c29a9048c4864d89915b29f4f39330e4/uuid/7d41086473994122bc61d4da73ccb229/thumbnail",
-                refd: "AR-2008-8.1.3.54",
+                mediaThumb: "https://fordheritagevaultmedia.com/api/links/c29a9048c4864d89915b29f4f39330e4/uuid/1418ed4f0f2e46efa6ff0b1c832e3d54/thumbnail",
+                refd: "AR-2008-8.1.7.504",
                 scope: undefined,
-                title: "1956 Ford World Headquarters at night neg C793",
+                title: "2007 Ford Airstream concept neg CN336950-041",
             },
             {
-                mediaThumb: "https://fordheritagevaultmedia.com/api/links/c29a9048c4864d89915b29f4f39330e4/uuid/5f69065af00f46169c57d27a924ba2d4/thumbnail",
-                refd: "AR-2000-213654.81.39.23",
+                mediaThumb: "https://fordheritagevaultmedia.com/api/links/c29a9048c4864d89915b29f4f39330e4/uuid/ea9ac9e15942454caf45ea78787522c9/thumbnail",
                 scope: undefined,
-                title: "1972 Ford Dagenham Assembly Plant vehicles during paint process CN UK 1972-595-0056",
+                title: "1965 Ford Cougar II concept on scenic road neg CN4228-008",
             }, {
-                mediaThumb: "https://fordheritagevaultmedia.com/api/links/c29a9048c4864d89915b29f4f39330e4/uuid/77cc6f47bdd94afd9c95b43df024a2e2/thumbnail",
-                refd: "AR-2008-8.1.2.1682",
+                mediaThumb: "https://fordheritagevaultmedia.com/api/links/c29a9048c4864d89915b29f4f39330e4/uuid/d0fa37a8f4804ac2a3f8f292db4f2669/thumbnail",
+                refd: "AR-2008-8.1.11.2231",
                 scope: undefined,
-                title: "1925 Ford Airport blimp mooring mast and airplane neg 45006",
+                title: "1955 Ford Mystere concept with scenic trees neg C612-1dw",
             },
             {
-                mediaThumb: "https://fordheritagevaultmedia.com/api/links/c29a9048c4864d89915b29f4f39330e4/uuid/e15baf3fe300471a9987a59a44f80abb/thumbnail",
-                refd: "AR-2008-8.1.10.8002",
+                mediaThumb: "https://fordheritagevaultmedia.com/api/links/c29a9048c4864d89915b29f4f39330e4/uuid/396d21b33f44451eba84c012461b12b1/thumbnail",
+                refd: "AR-2008-8.1.13.305",
                 scope: undefined,
-                title: "2003 Ford Cologne Body and Assembly Plant machine timelapse neg CN336043-006",
+                title: "1958 Ford Nucleon concept in studio neg CN4032-008a",
             }, {
-                mediaThumb: "https://fordheritagevaultmedia.com/api/links/c29a9048c4864d89915b29f4f39330e4/uuid/390b51e99f0744fa9ce151432bc5b695/thumbnail",
-                refd: "AR-2008-8.1.10.8061",
+                mediaThumb: "https://fordheritagevaultmedia.com/api/links/c29a9048c4864d89915b29f4f39330e4/uuid/c54d61181f5b433aabfc757985755c3d/thumbnail",
+                refd: "AR-2008-8.1.13.295",
                 scope: undefined,
-                title: "2009 Completed Ford Taurus at Chicago Assembly Plant",
+                title: "1995 Ford GT90 Side Profile CN315025-25",
             }, {
-                mediaThumb: "https://fordheritagevaultmedia.com/api/links/c29a9048c4864d89915b29f4f39330e4/uuid/67c1c856cbc2474693c8d5ec030bdf1e/thumbnail",
-                refd: "AR-2008-8.1.1.3220",
+                mediaThumb: "https://fordheritagevaultmedia.com/api/links/c29a9048c4864d89915b29f4f39330e4/uuid/4647e2e01f0540418a9046f61b1b487b/thumbnail",
+                refd: "AR-2008-8.1.13.348",
                 scope: undefined,
-                title: "1953 Ford Rotunda Visitor Center interior neg C669-4",
+                title: "1970 Ford Maverick Estate concept car passenger side neg CN6006-106",
             }, {
-                mediaThumb: "https://fordheritagevaultmedia.com/api/links/c29a9048c4864d89915b29f4f39330e4/uuid/b6d098a3c6d34eb3ba47749f0e8bf5ff/thumbnail",
-                refd: "AR-2008-8.1.2.1162",
+                mediaThumb: "https://fordheritagevaultmedia.com/api/links/c29a9048c4864d89915b29f4f39330e4/uuid/1ebbd7d0424b43e585f7ea1a52872361/thumbnail",
+                refd: "AR-2008-8.1.11.1629",
                 scope: undefined,
-                title: "1961 Ford Rouge Complex exterior neg C1049-4",
+                title: "c 1955 Ford Volante concept in studio neg CN5023-001",
             },
         ]
         TRENDING_NOW_RECORD.map((e, i) => new TrendingNow(e, $('.trendingNow').eq(i)).initUIManual(e))
