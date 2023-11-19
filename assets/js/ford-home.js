a7516bb083ecb35a6a9a623bedefebdc31bbe6c4
opacteam <mike@minisisinc.com>
2023-11-19 01:25:30 -0500

    push to live

--- assets/js/ford-home.js
+++ assets/js/ford-home.js
@@ -4,21 +4,21 @@ $(document).ready(function () {
         // const LATEST_ARCHIVE_REFD = ['AR-96-212010.5423', 'AR-2008-8.1.4.555', 'AR-2008-8.1.2.2049'];
         const LATEST_ARCHIVE_RECORD = [
             {
-                mediaThumb: "https://fordheritagevaultmedia.com/api/links/c29a9048c4864d89915b29f4f39330e4/uuid/9d0507cf6150430eaf1ad2ac780cf6c2/thumbnail",
-                refd: "AR-2008-8.1.13.410",
+                mediaThumb: "https://fordheritagevaultmedia.com/api/links/c29a9048c4864d89915b29f4f39330e4/uuid/8b5030fbf843487b81bf64a5c5ac12fb/thumbnail",
+                refd: "AR-2008-8.1.11.3087",
                 scope: undefined,
-                title: "2003 Ford Model U concept car interior CN335041-337"
+                title: "1962 Ford Seattle-ite XXI concept in studio with open door CN1124-6"
             },
             {
-                mediaThumb: "https://fordheritagevaultmedia.com/api/links/c29a9048c4864d89915b29f4f39330e4/uuid/2bd63756fcb641d4b4de5a01e24db660/thumbnail",
-                refd: "AR-96-212010.6880",
+                mediaThumb: "https://fordheritagevaultmedia.com/api/links/c29a9048c4864d89915b29f4f39330e4/uuid/f4db8bbbb11c4a3db12cc67cbe4b5948/thumbnail",
+                refd: "AR-88-108165.437.37",
                 scope: undefined,
-                title: "c 1960 Levacar concept brochure AR-2004-72-12262"
+                title: "The Seattle-ite XXI was Displayed at the Seattle World Fair"
             }, {
-                mediaThumb: "https://fordheritagevaultmedia.com/api/links/c29a9048c4864d89915b29f4f39330e4/uuid/c9219a4a89fb4ccd8b0b7ae49f614f23/thumbnail",
-                refd: "AR-2008-8.1.11.2008",
+                mediaThumb: "https://fordheritagevaultmedia.com/api/links/c29a9048c4864d89915b29f4f39330e4/uuid/af64655573434213b011995ab3145d46/thumbnail",
+                refd: "AR-2008-8.1.11.3088",
                 scope: undefined,
-                title: "1962 Ford Mustang I concept on scenic road neg CN2042-1",
+                title: "1962 Ford Seattle-ite XXI concept rear view with open door in studio CN1124-7",
             }
         ]
         LATEST_ARCHIVE_RECORD.map((e, i) => new LatestArchive(e, $('.latestArchive').eq(i)).initUIManual(e))
@@ -26,42 +26,43 @@ $(document).ready(function () {
         const TRENDING_NOW_REFD = ['AR-65-90.1249.7', 'AR-2008-8.1.2.2447', 'AR-96-212010.5443', 'AR-2008-8.1.2.4143', 'AR-2008-8.1.7.257', 'AR-96-212010.6325', 'AR-96-212010.5592'];
         const TRENDING_NOW_RECORD = [
             {
-                mediaThumb: "https://fordheritagevaultmedia.com/api/links/c29a9048c4864d89915b29f4f39330e4/uuid/1418ed4f0f2e46efa6ff0b1c832e3d54/thumbnail",
-                refd: "AR-2008-8.1.7.504",
+                mediaThumb: "https://fordheritagevaultmedia.com/api/links/c29a9048c4864d89915b29f4f39330e4/uuid/dd9435581bb640e2a9acab47106f0733/thumbnail",
+                refd: "AR-2008-8.1.4.5602",
                 scope: undefined,
-                title: "2007 Ford Airstream concept neg CN336950-041",
+                title: "1966 Ford Fairlane GT-X two-door hardtop concept car neg CN3802-156",
             },
             {
-                mediaThumb: "https://fordheritagevaultmedia.com/api/links/c29a9048c4864d89915b29f4f39330e4/uuid/ea9ac9e15942454caf45ea78787522c9/thumbnail",
+                mediaThumb: "https://fordheritagevaultmedia.com/api/links/c29a9048c4864d89915b29f4f39330e4/uuid/6600704c9abd4b598d67cb8fafe9dad8/thumbnail",
+                refd:"AR-2008-8.1.11.1639",
                 scope: undefined,
-                title: "1965 Ford Cougar II concept on scenic road neg CN4228-008",
+                title: "1960 Ford Typhoon II (Goliath) tractor concept model Side View neg 145786-13.jpg",
             }, {
-                mediaThumb: "https://fordheritagevaultmedia.com/api/links/c29a9048c4864d89915b29f4f39330e4/uuid/d0fa37a8f4804ac2a3f8f292db4f2669/thumbnail",
-                refd: "AR-2008-8.1.11.2231",
+                mediaThumb: "https://fordheritagevaultmedia.com/api/links/c29a9048c4864d89915b29f4f39330e4/uuid/a51683d439ed44088fc070fb693e5663/thumbnail",
+                refd: "AR-2008-8.1.8.1377",
                 scope: undefined,
-                title: "1955 Ford Mystere concept with scenic trees neg C612-1dw",
+                title: "2002 Ford FR-100 concept truck neg 002 AR-2001-213703",
             },
-            {
-                mediaThumb: "https://fordheritagevaultmedia.com/api/links/c29a9048c4864d89915b29f4f39330e4/uuid/396d21b33f44451eba84c012461b12b1/thumbnail",
-                refd: "AR-2008-8.1.13.305",
+           {
+                mediaThumb: "https://fordheritagevaultmedia.com/api/links/c29a9048c4864d89915b29f4f39330e4/uuid/a1e2f7837afd4f1495eee6a439775dd6/thumbnail",
+                refd: "AR-2008-8.1.11.2567",
                 scope: undefined,
-                title: "1958 Ford Nucleon concept in studio neg CN4032-008a",
+                title: "1969 Mercury Cougar El Gato concept car Side View with Mountains in Background neg  CN5865-8",
             }, {
-                mediaThumb: "https://fordheritagevaultmedia.com/api/links/c29a9048c4864d89915b29f4f39330e4/uuid/c54d61181f5b433aabfc757985755c3d/thumbnail",
-                refd: "AR-2008-8.1.13.295",
+                mediaThumb: "https://fordheritagevaultmedia.com/api/links/c29a9048c4864d89915b29f4f39330e4/uuid/4b389d16319f4df9a4545f591822a16e/thumbnail",
+                refd: "AR-2008-8.1.11.2576",
                 scope: undefined,
-                title: "1995 Ford GT90 Side Profile CN315025-25",
+                title: "1969 Mercury Montego Super Spoiler concept car Overhead View with Top Down neg CN5644-2",
             }, {
-                mediaThumb: "https://fordheritagevaultmedia.com/api/links/c29a9048c4864d89915b29f4f39330e4/uuid/4647e2e01f0540418a9046f61b1b487b/thumbnail",
-                refd: "AR-2008-8.1.13.348",
-                scope: undefined,
-                title: "1970 Ford Maverick Estate concept car passenger side neg CN6006-106",
-            }, {
-                mediaThumb: "https://fordheritagevaultmedia.com/api/links/c29a9048c4864d89915b29f4f39330e4/uuid/1ebbd7d0424b43e585f7ea1a52872361/thumbnail",
+                mediaThumb: "https://fordheritagevaultmedia.com/api/links/c29a9048c4864d89915b29f4f39330e4/uuid/ca92efed9f4447dcbaeb5d5132132159/thumbnail",
                 refd: "AR-2008-8.1.11.1629",
                 scope: undefined,
-                title: "c 1955 Ford Volante concept in studio neg CN5023-001",
-            },
+                title: "1979 Ford Mustang IMSA concept car driver side neg CN25593-4",
+            },{
+                mediaThumb: "https://fordheritagevaultmedia.com/api/links/c29a9048c4864d89915b29f4f39330e4/uuid/5cfc02f475d84e0cb1ce771007e12f64/thumbnail",
+                refd: "AR-2008-8.1.13.903",
+                scope: undefined,
+                title: "1954 Lincoln Capri Huntsman convertible concept passenger side profile neg C879-3",
+            }
         ]
         TRENDING_NOW_RECORD.map((e, i) => new TrendingNow(e, $('.trendingNow').eq(i)).initUIManual(e))
 
